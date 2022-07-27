8592a7579de22e7dab931f193ab4d693f31dd9b3
Trytonchik <107284356+Trytonchik@users.noreply.github.com>
2022-07-27 03:58:08 +0300

    Add files via upload

---
+++ Новая папка (7)/README.md
@@ -0,0 +1,1 @@
+# unfeelsite.github.io
---
+++ Новая папка (7)/js/config.js
@@ -0,0 +1,45 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 2.5, 
+
+  // Заголовок
+  title: 'Tryton#6309',
+
+  // Социальные сети
+  social: {
+    telegram: '@Trytonik',
+    steam: 'STEAM_1:1:611363326',
+    vk: 'trytonik', 
+  },
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#ff0000',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#ff0000',
+  // Цвет тени кнопки play
+  playButtonShadowColor: 'rgba(255, 0, 55, 5.0)',
+
+
+  // Цвет заголовка
+  titleColor: '#FF0000',
+  // Цвет тени загаловка
+  titleShadowColor: 'rgba(255, 0, 55, 0.6)',
+
+
+  // Текст копирайта
+  copyrightText: 'dev - Tryton#6309 ',
+  // Цвет копирайта
+  copyrightTextColor: '#f5323c',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#FF0000',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.2)',
+}
